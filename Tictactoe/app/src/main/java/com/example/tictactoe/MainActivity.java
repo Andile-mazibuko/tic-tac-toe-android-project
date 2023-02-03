16e541990b8ecd7775726c181bdf7ffc8d25ec6e
Andile-mazibuko <andilelethumusa@gmail.com>
2023-02-04 00:38:04 +0200

    started gameplay implemantation

--- Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -20,21 +20,47 @@ public class MainActivity extends AppCompatActivity {
         num+=1;
         Button button = (Button) v;
         button.setEnabled(false);
+        String player = "X";
+
+
 
         if(num%2 != 0)
         {
-            button.setText("X");
+
+            button.setText(player);
         }else
         {
-            button.setText("O");
+            player = "O";
+            button.setText(player);
 
         }
+        check(player);
 
-
-
-       // Toast.makeText(this, ""+buttonId, Toast.LENGTH_SHORT).show();
+      //  Toast.makeText(this, ""+button.getText(), Toast.LENGTH_SHORT).show();
         
         //return buttonId;
+    }
+    public void check(String player)
+    {
+        //winning combinations  {00,01,02},{00,10,20},{10,11,12},{20,21,22}
+        Button btn00 = (Button) findViewById(R.id.btn00);
+        Button btn01 = (Button) findViewById(R.id.btn01);
+        Button btn02 = (Button) findViewById(R.id.btn02);
+
+        Button btn10 = (Button) findViewById(R.id.btn10);
+        Button btn11 = (Button) findViewById(R.id.btn11);
+        Button btn12 = (Button) findViewById(R.id.btn12);
+
+        Button btn20 = (Button) findViewById(R.id.btn20);
+        Button btn21 = (Button) findViewById(R.id.btn21);
+        Button btn22 = (Button) findViewById(R.id.btn22);
+
+        if((btn00.getText().equals(player) && btn01.getText().equals(player) && btn02.getText().equals(player)) ||
+                (btn00.getText().equals(player) && btn11.getText().equals(player) && btn22.getText().equals(player)) )
+        {
+            Toast.makeText(this, "Player{ " +player+" } won this round", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
