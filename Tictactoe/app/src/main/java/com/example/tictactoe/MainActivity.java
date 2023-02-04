0fcd97a079f8ad5329c0fee0577bbe8062db8ca4
Andile-mazibuko <andilelethumusa@gmail.com>
2023-02-04 18:25:54 +0200

    Increased font size

--- Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -10,11 +10,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Integer num = 0,playerX = 0, playerO = 0;
-
+    Button[] buttons;
+    Button btn00,btn01,btn02,btn10,btn11,btn12,btn20,btn21,btn22;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Tic tac ");
     }
     public void buttonClicked(View v)
     {
@@ -40,18 +42,18 @@ public class MainActivity extends AppCompatActivity {
     public void check(String player)
     {
         //winning combinations  {00,01,02},{00,10,20},{10,11,12},{20,21,22}
-        Button btn00 = (Button) findViewById(R.id.btn00);
-        Button btn01 = (Button) findViewById(R.id.btn01);
-        Button btn02 = (Button) findViewById(R.id.btn02);
+         btn00 = (Button) findViewById(R.id.btn00);
+         btn01 = (Button) findViewById(R.id.btn01);
+         btn02 = (Button) findViewById(R.id.btn02);
 
-        Button btn10 = (Button) findViewById(R.id.btn10);
-        Button btn11 = (Button) findViewById(R.id.btn11);
-        Button btn12 = (Button) findViewById(R.id.btn12);
+         btn10 = (Button) findViewById(R.id.btn10);
+         btn11 = (Button) findViewById(R.id.btn11);
+         btn12 = (Button) findViewById(R.id.btn12);
 
-        Button btn20 = (Button) findViewById(R.id.btn20);
-        Button btn21 = (Button) findViewById(R.id.btn21);
-        Button btn22 = (Button) findViewById(R.id.btn22);
-        Button[] buttons = {btn00,btn01,btn02,btn10,btn11,btn12,btn20,btn21,btn22};
+         btn20 = (Button) findViewById(R.id.btn20);
+         btn21 = (Button) findViewById(R.id.btn21);
+         btn22 = (Button) findViewById(R.id.btn22);
+        buttons = new Button[]{btn00,btn01,btn02,btn10,btn11,btn12,btn20,btn21,btn22};
 
         if((btn00.getText().equals(player) && btn01.getText().equals(player) && btn02.getText().equals(player)) ||
                 (btn10.getText().equals(player) && btn11.getText().equals(player) && btn12.getText().equals(player)) ||
@@ -73,6 +75,16 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+    public void replay(View v)
+    {
+
+        for(int i = 0; i < buttons.length; i++)
+        {
+            buttons[i].setText("");
+            buttons[i].setEnabled(true);
+        }
+        num =0;
     }
 // method to execute after the player has played 3 times
 
