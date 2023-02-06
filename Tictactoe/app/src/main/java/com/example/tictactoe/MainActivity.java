996df639969eb791a91cc493469ea51a2073dee2
Andile-mazibuko <andilelethumusa@gmail.com>
2023-02-06 18:27:56 +0200

    Launcher icon changed and some color changes

--- Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ Tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Tic tac ");
+        setTitle("Tic Tac Toe");
     }
     public void buttonClicked(View v)
     {
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) v;
         button.setEnabled(false);
         String player = "X";
-
+        ((Button)v).setTextColor(Color.CYAN);
         if(num%2 != 0)
         {
             button.setText(player);
@@ -41,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void check(String player)
     {
+        int winningColor = Color.GREEN;
+        int lossingColor = Color.RED;
         //winning combinations  {00,01,02},{00,10,20},{10,11,12},{20,21,22}
          btn00 = (Button) findViewById(R.id.btn00);
          btn01 = (Button) findViewById(R.id.btn01);
@@ -53,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
          btn20 = (Button) findViewById(R.id.btn20);
          btn21 = (Button) findViewById(R.id.btn21);
          btn22 = (Button) findViewById(R.id.btn22);
-        buttons = new Button[]{btn00,btn01,btn02,btn10,btn11,btn12,btn20,btn21,btn22};
+
+         buttons = new Button[]{btn00,btn01,btn02,btn10,btn11,btn12,btn20,btn21,btn22};
 
         if((btn00.getText().equals(player) && btn01.getText().equals(player) && btn02.getText().equals(player)) ||
                 (btn10.getText().equals(player) && btn11.getText().equals(player) && btn12.getText().equals(player)) ||
@@ -68,9 +73,17 @@ public class MainActivity extends AppCompatActivity {
 
         )
         {
+
             Toast.makeText(this, "Player{ " +player+" } won this round", Toast.LENGTH_SHORT).show();
             for(int i = 0; i < buttons.length; i++)
             {
+                if(buttons[i].getText().equals(player))
+                {
+                    buttons[i].setBackgroundColor(winningColor);
+                }else if(!buttons[i].getText().equals(""))
+                {
+                    buttons[i].setBackgroundColor(lossingColor);
+                }
                 buttons[i].setEnabled(false);
             }
         }
@@ -78,15 +91,17 @@ public class MainActivity extends AppCompatActivity {
     }
     public void replay(View v)
     {
-
+        int color = Color.parseColor("#FFBB86FC");
         for(int i = 0; i < buttons.length; i++)
         {
+
             buttons[i].setText("");
+            buttons[i].setBackgroundColor(color);
             buttons[i].setEnabled(true);
+
         }
         num =0;
     }
-// method to execute after the player has played 3 times
 
 
 }
